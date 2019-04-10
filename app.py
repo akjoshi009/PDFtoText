3258f87a5dbe2a6536bc021015372a0a77857290
Sachin Anbhule <31981860+akjoshi009@users.noreply.github.com>
2019-04-10 19:49:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import io
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.config['suppress_callback_exceptions']=True
-pytesseract.pytesseract.tesseract_cmd = '/app/vendor/tesseract-ocr/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = '/app/vendor/tesseract-ocr/bin/tesseract/'
 
 
 server = app.server
